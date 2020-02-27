441023cf9ad3ffce900391057f295ddb77a0bbcb
alumno <alumno@utnfra.com.ar>
2020-02-26 21:49:29 -0300

    Modelo Parcial

--- Modelo_Parcial_Ingreso/Modelo_Parcial_Ingreso/dos.js
+++ Modelo_Parcial_Ingreso/Modelo_Parcial_Ingreso/dos.js
@@ -1,46 +1,86 @@
 function mostrar()
 {
-    var persona=0;
-    var nombre;
+//Declaro variables
     var contador=0;
-    var genero=0;
-    var edad=0;
-    var peso=0;
+    var nombre;
+    var genero;
+    var edad;
+    var peso;
     var acumulador=0;
     var acumGenero;
     var acumEdad=0;
     var acumPeso=0;
+    var cantEdadMayor25=0;
     var cantPersPesoM80=0;
-    var personaMenosPesada=0;
-    var promedio=0;
-
+    var personaMenosPesada;
+    var pesoMinimo;
+    var promedioEdad=0;
+    var esPrimeraIteracion;
+    var cantpersonas=0;
+//Inicicalizo variables y valido
     for (contador = 0; contador< 4; contador++) {
 
         nombre=prompt("Ingrese el nombre de las 4 personas:");
-        acumulador=nombre +nombre;
-        }
-        alert(acumulador);
-   
-        genero=prompt("Ingrese el genero:");
-        while(genero!="masculino" || genero!="femenino" || genero!="otro")
+
+        genero=prompt("Ingrese el genero (femenino/masculino/otros):").toLowerCase;
+
+        while(genero!="masculino" && genero!="femenino" && genero!="otro")
         {
-            genero=prompt("Ingrese el genero correcto:");   
-            acumgenero=genero;     
+            genero=prompt("Ingrese el genero correcto:").toLowerCase;      
         }
 
-        edad=parseInt(prompt("Ingrese la edad:"));
-        while(edad<18 && edad>100)
+        edad=parseInt(prompt("Ingrese la edad (entre 18 y 100):"));
+
+        // Entiendo que se incluya al 18 y al 100 en los valores permitidos.
+        while(isNaN(edad) || edad<18 || edad>100)
         {
             edad=parseInt(prompt("Ingrese la edad correcta:")); 
-            acumEdad=edad;    
+   
         }
 
         peso=parseInt(prompt("Ingrese el peso:"));
-        while(peso=isNaN && peso<1)
+
+        // Entiendo que no se incluya al 1.
+        while(isNaN(peso) || peso<= 1)
         {
             peso=parseInt(prompt("Ingrese el peso correcto:")); 
-            acumPeso=peso;       
+
+        }    
+        //Punto A
+        if(edad>=25)
+        {
+            acumEdad += edad;
+            cantEdadMayor25++;
         }
-        
-        
+        //Punto B
+        if(esPrimeraIteracion || peso< pesoMinimo)
+        {
+            pesoMinimo=peso;
+            personaMenosPesada=nombre;
+        }
+        //entiendo que 80 no esta incluido.
+        if(genero!="masculino" || peso>80)
+        {
+            cantpersonas ++;
+        }
+        esPrimeraIteracion=false;
+        }
+
+        //proceso los datos
+        if(cantEdadMayor25>0)
+        {
+            promedioEdad= acumEdad/cantEdadMayor25;
+            respuesta += "El promedio de edades mayores a 25 es " + promedioEdad;
+        }
+        else
+        {
+            respuesta +="No se ingresaron edades mayores a 25";
+        }
+        respuesta += personaMenosPesada + "Es la persona menos pesada y pesa: " +pesoMinimo +"kg.<br>";
+        respuesta +="La cantidad de personas de genero distinto de masculino o peso mayor a 80 es: " + cantpersonas;
+
+
+        //muestro los resultados
+        document.write(respuesta);
+     
 }
--- Modelo_Parcial_Ingreso/Modelo_Parcial_Ingreso/tres.js
+++ Modelo_Parcial_Ingreso/Modelo_Parcial_Ingreso/tres.js
@@ -1,4 +1,19 @@
+/*Realizar el algoritmo que permita ingresar el nombre de un país,
+cantidad de habitantes en millones entre 1 y 270 (validar que sea un número en ese rango),
+y la temperatura mínima que se registra en su territorio (validar que sea un número entre -50 y 50)
+hasta que el usuario quiera
+e informar al terminar el ingreso por document.write:
+a) La cantidad de temperaturas impares.
+b) El nombre del pais con más habitantes.
+c) La cantidad de paises por debajo de los 20 grados.
+d) El promedio de habitantes entre los paises ingresados.
+f) La temperatura máxima ingresada, y nombre del pais que registro esa temperatura.
+*/
 function mostrar()
 {
+    var pais;
+    var cantHabitantes;
+    var temperaturaMin;
 
+    
 }
